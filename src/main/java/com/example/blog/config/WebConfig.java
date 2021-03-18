4f9058e8434dadab9e06078e5b055cab6dd9e28b
kserafimova <kserafimova@phoenixit.ru>
2021-03-18 19:42:21 +0300

    add resource handler for swagger

--- src/main/java/com/example/blog/config/WebConfig.java
+++ src/main/java/com/example/blog/config/WebConfig.java
@@ -14,6 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
+
 }
